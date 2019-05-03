8bc3ec33bc3be371b248bbd1d27c546e9010f5d0
Travi5JAB <travis841235@gmail.com>
2019-05-03 14:50:47 -0700

    initial commit

--- react.js
+++ react.js
@@ -1,8 +1,37 @@
+console.log("Change");
 // React Assessments
 
 // 1. Write a simple React component that simply prints "I am a component" to the screen. Be sure to include all necessary imports, exports, etc...
+import React, { Component }from 'react';
+import './App.css';
 
-// 2. Practice With Loops: In React, we often use the map function to iterate over an array held in state. To get more practice with this, write a vanilla javascript for loop that outputs the same thing as the map function below. 
+
+class Print extends Component {
+    constructor(){
+        this.state{
+            compLog: "I am a component"
+        }
+    }
+    render(){
+        let {compLog} = this.state
+      return (
+        <div className="App">
+            <h1>{complog}</h1>
+        </div>
+      );
+    }
+}
+
+export default Print;
+
+
+
+}
+
+
+////////////////////////////////////////////////////////////////////////////
+
+// 2. Practice With Loops: In React, we often use the map function to iterate over an array held in state. To get more practice with this, write a vanilla javascript for loop that outputs the same thing as the map function below.
 
 
 var stuffArray = [42, "Arthur Dent", 1978, "Zaphod", "Vogon", "Marvin, the Paranoid Android"]
@@ -13,11 +42,12 @@ stuffArray.map(function(el, i){
 
 // your for loop here, you can use the same stuffArray array
 
-
+////////////////////////////////////////////////////////////////////////////
 // 3. Basic sorting: Find the cheapest price -- you are given a list of prices and need to find the signle lowest price from the array. Write a function that takes in an array of numbers, and returns the lowest price
 
 var steal = function(){
 
   // your logic here
-  
+
 }
+////////////////////////////////////////////////////////////////////////////
